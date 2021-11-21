db7fadd8a57a354961ca3d37112660befa50e90e
AizOfMulia <aizofmulia@gmail.com>
2021-11-21 16:44:00 +0800

    added separate fixed footer for smaller height webpage
    Signed-off-by: AizOfMulia <aizofmulia@gmail.com>

---
+++ includes/footer_fixed.php
@@ -0,0 +1,8 @@
+    <div class="container-fluid bg-darkblue fixed-bottom">
+        <footer class="footer mt-auto py-3 text-light">
+            <p>Copyright 2021 Murni Bus Ticketing</p>
+        </footer>
+    </div>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
